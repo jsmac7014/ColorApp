6cb555a37462ffaf0d6ba83a61105c44e404591b
JungWoo Song <jsmac7014@gmail.com>
2018-01-11 23:40:24 +0900

    error fixed

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const ejs = require('ejs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = require('./routes/routes.js');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
 
 app.use(express.static('public'));
 app.use('/',router);
